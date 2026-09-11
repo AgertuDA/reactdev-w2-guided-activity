a08bc5c50e0b433e7dbbc55ed2c40bc747141ffb
AgertuDA <agertudiriba@gmail.com>
2026-09-11 08:36:44 +0200

    style: add MyComponent styling

--- my-first-react-app/src/App.tsx
+++ my-first-react-app/src/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import MyComponent from "./MyComponent";
+import MyComponent from "./MyComponent";;
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="my-component">
       <MyComponent />
     </div>
   );
--- my-first-react-app/src/MyComponent.tsx
+++ my-first-react-app/src/MyComponent.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./MyComponent.css";
 
 const MyComponent: React.FC = () => {
   return (
